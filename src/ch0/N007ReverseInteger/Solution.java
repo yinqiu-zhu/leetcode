d1c2e0d94347d82850f9509301edd232a967afa1
Yinqiu Zhu <zyqhlz@gmail.com>
2019-08-11 13:10:32 -0700

    Add old file in my surface

---
+++ src/ch0/N007ReverseInteger/Solution.java
@@ -0,0 +1,18 @@
+package N007ReverseInteger;
+
+class Solution {
+    public int reverse(int x) {
+        int res = 0;
+        while (x != 0   ){
+            if (res < Integer.MAX_VALUE / 10 && res > Integer.MIN_VALUE / 10)
+                return 0;
+            res = res * 10 + x % 10;
+            x = x / 10;
+        }
+    return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(-1 % 2);
+    }
+}
