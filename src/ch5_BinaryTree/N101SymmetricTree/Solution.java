32b86ab332767ba750687ff7f1273f4561dffeba
Yinqiu Zhu <zyqhlz@gmail.com>
2019-08-21 21:11:34 -0700

    Merge from win

--- src/ch5_BinaryTree/N101SymmetricTree/Solution.java
+++ src/ch5_BinaryTree/N101SymmetricTree/Solution.java
@@ -1,4 +1,21 @@
 package ch5_BinaryTree.N101SymmetricTree;
 
+import ch5_BinaryTree.TreeNode;
+
 public class Solution {
+
+  public boolean isSymmetric(TreeNode root) {
+
+    if (root == null) return true;
+
+    return lowLevelComparision(root.left, root.right);
+  }
+
+  public boolean lowLevelComparision(TreeNode left, TreeNode right){
+    if (left == null && right == null) return true;
+    if (left == null || right == null) return false;
+    return left.val == right.val &&
+        lowLevelComparision(left.left, right.right) &&
+            lowLevelComparision(left.right, right.left);
+  }
 }
