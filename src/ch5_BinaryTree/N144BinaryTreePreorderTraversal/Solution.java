7d000167601c4c23494fad0fe69310817baba345
Yinqiu Zhu <zyqhlz@gmail.com>
2019-08-21 21:04:18 -0700

    Changes in a Mac.

--- src/ch5_BinaryTree/N144BinaryTreePreorderTraversal/Solution.java
+++ src/ch5_BinaryTree/N144BinaryTreePreorderTraversal/Solution.java
@@ -1,4 +1,20 @@
 package ch5_BinaryTree.N144BinaryTreePreorderTraversal;
 
+import java.util.List;
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 public class Solution {
+  public List<Integer> preorderTraversal(TreeNode root) {
+
+  }
+
 }
---
+++ src/ch5_BinaryTree/N144BinaryTreePreorderTraversal/TreeNode.java
@@ -0,0 +1,4 @@
+package ch5_BinaryTree.N144BinaryTreePreorderTraversal;
+
+public class TreeNode {
+}
