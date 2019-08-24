758643ee14e5eac5227e7984832e05314f97c7dc
Yinqiu Zhu <zyqhlz@gmail.com>
2019-08-23 20:10:28 -0700

    Regular update.

---
+++ src/ch6_NaryTree/EncodeNaryTreeToBinaryTree/TreeNode.java
@@ -0,0 +1,8 @@
+package ch6_NaryTree.EncodeNaryTreeToBinaryTree;
+
+public class TreeNode {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int x) { val = x; }
+  }
