69777be81739d8026f9a36808447aa140775bf21
Yinqiu Zhu <zyqhlz@gmail.com>
2020-03-25 14:36:33 -0700

    Add a solution of N070.

---
+++ src/first2last/N001099/N088/Solution.java
@@ -0,0 +1,14 @@
+package first2last.N001099.N088;
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int p1 = 0, p2 = 0, p3 = m - 1;
+        while (p1 < nums1.length){
+            while (p2 < n){
+                if (nums1[p1] < nums2[p2] && nums1[p3] < nums1[p1] )
+            }
+        }
+
+    }
+}
