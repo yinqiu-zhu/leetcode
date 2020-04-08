f9bc0cc4cb27f83859774f8fac6dd5d612811cb2
Yinqiu Zhu <zyqhlz@gmail.com>
2020-04-08 14:07:41 -0700

    Add a solution of N876.

---
+++ src/N8/N876MiddleOfTheLinkedList/ListNode.java
@@ -0,0 +1,7 @@
+package N8.N876MiddleOfTheLinkedList;
+
+public class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) { val = x; }
+}
---
+++ src/N8/N876MiddleOfTheLinkedList/Solution.java
@@ -0,0 +1,22 @@
+package N8.N876MiddleOfTheLinkedList;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+
+        ListNode fast = head, low = head;
+
+        while (fast != null && fast.next != null){
+            fast = fast.next.next;
+            low = low.next;
+        }
+        return low;
+    }
+}
