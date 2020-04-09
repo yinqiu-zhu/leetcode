3dd54cb054f976ba86cfd1139692bc1e6dc55079
Yinqiu Zhu <zyqhlz@gmail.com>
2020-04-09 14:26:14 -0700

    Add a solution of N844.

---
+++ src/N8/N844BackspaceStringCompare/Solution.java
@@ -0,0 +1,32 @@
+package N8.N844BackspaceStringCompare;
+
+import java.util.Stack;
+
+class Solution {
+    public boolean backspaceCompare(String S, String T) {
+        Stack<Character> stackS = new Stack<>(), stackT = new Stack<>();
+        for (char ch : S.toCharArray()){
+            if (ch == '#'){
+                if (!stackS.empty()) stackS.pop();
+            }
+            else stackS.push(ch);
+        }
+        for (char ch : T.toCharArray()){
+            if (ch == '#') {
+                if (!stackT.empty()) stackT.pop();
+            }
+            else stackT.push(ch);
+        }
+        while (!stackS.empty()){
+            if (stackT.empty() || stackS.pop() != stackT.pop())
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "ab##a", t = "c#d#";
+        Solution one = new Solution();
+        one.backspaceCompare(s,t);
+    }
+}
---
+++ src/first2last/N0/N005/Solution.java
@@ -0,0 +1,4 @@
+package first2last.N0.N005;
+
+public class Solution {
+}
