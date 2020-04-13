652ef5a970d44e9831af8918763f975952645b30
Yinqiu Zhu <zyqhlz@gmail.com>
2020-04-13 14:45:57 -0700

    Add a solution of N1046.

---
+++ src/first2last/N10/N1046/Solution.java
@@ -0,0 +1,14 @@
+package first2last.N10.N1046;
+
+import java.util.PriorityQueue;
+
+public class Solution {
+    public int lastStoneWeight(int[] A) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b)-> b - a);
+        for (int a : A)
+            pq.offer(a);
+        while (pq.size() > 1)
+            pq.offer(pq.poll() - pq.poll());
+        return pq.poll();
+    }
+}
