fcf98e6e3b9fd7c9454d678efd1bfd6114bec237
Yinqiu Zhu <zyqhlz@gmail.com>
2020-04-13 17:24:26 -0700

    Add a solution of N005.

--- src/first2last/N0/N005/Solution.java
+++ src/first2last/N0/N005/Solution.java
@@ -1,26 +1,46 @@
 package first2last.N0.N005;
 
-
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
-    //DP solution
-    Map<String,int[]> map = new HashMap<>();
-    char[] s;
-    public String longestPalindrome(String s) {
-        this.s = s.toCharArray();
 
+    //DP solution
+
+    public String longestPalindrome(String s) {
+
+        if (s == null || s.length() == 0) return "";
+
+        Set<String> set = new HashSet<>();
+
+        final int LEN = s.length();
+        int start = 0, end = 0;
+        boolean[][] dp = new boolean[LEN][LEN];
+
+        for (int i = 0; i < LEN; i++ ){
+            for (int j = i; j >= 0; j--){
+
+                if (set.contains(i+"|"+j)) break;
+
+                boolean temp  =  s.charAt(i) == s.charAt(j);
+                if ( i == j )
+                    dp[i][j] = true;
+                else if (i - 1 == j ) dp[i][j] = temp;
+                else if ( temp && dp[i-1][j+1]) dp[i][j] = true;
+
+                set.add(i+"|"+j);
+                if (dp[i][j] && i - j > end - start){
+                    end = i;
+                    start = j;
+                }
+            }
+        }
+        return s.substring(start,end+1);
     }
 
-    private int[] dp(int i, int j){
-
-        if ( i == j ) return new {i,i};
-
-        String temp = ;
-        if (map.containsKey(temp)) return map.get(temp);
-        else {
-
-        }
+    public static void main(String[] args) {
+        String a = "babad";
+        Solution one = new Solution();
+        System.out.printf(one.longestPalindrome(a));
     }
 }
